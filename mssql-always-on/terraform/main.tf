e1da2d7645ea65f26d338341595f1aba0e107f0c
Pavel Karpenko <karpenko.p.n@gmail.com>
2023-05-12 19:46:22 +0300

    Added terraform scripts for mssql-always-on deployment at the Nebius cloud with Nebius terraform Provider

--- mssql-always-on/terraform/main.tf
+++ mssql-always-on/terraform/main.tf
@@ -1,15 +1,14 @@
  
  terraform {
    required_providers {
-     yandex = {
-       source = "yandex-cloud/yandex"
+     nebius = {
+       source = "nebius-cloud/nebius"
      }
    }
-   required_version = ">= 0.13"
+   required_version = ">= 0.1.0"
  }
   
- provider "yandex" {
-   endpoint         = "api.cloudil.com:443"
+ provider "nebius" {
    token = "<ENTER_OAUTH_TOKEN>"
    cloud_id  = "<ENTER_CLOUD_ID>"
    folder_id = "<ENTER_FOLDER_ID>"
@@ -75,68 +74,68 @@ locals {
  }  
 
 /*creating VPC network with subnets*/
-resource "yandex_vpc_network" "ya-network" {
+resource "nebius_vpc_network" "ya-network" {
     name = "ya-network"
 }
 
-resource "yandex_vpc_subnet" "ya-sqlserver-rc1a" {
+resource "nebius_vpc_subnet" "ya-sqlserver-rc1a" {
   name       = "ya-sqlserver-rc1a"
   zone       = var.zone-a
-  network_id = "${yandex_vpc_network.ya-network.id}"
+  network_id = "${nebius_vpc_network.ya-network.id}"
   v4_cidr_blocks = ["192.168.1.0/28"]
 }
 
-resource "yandex_vpc_subnet" "ya-sqlserver-rc1b" {
+resource "nebius_vpc_subnet" "ya-sqlserver-rc1b" {
   name       = "ya-sqlserver-rc1b"
   zone       = var.zone-b
-  network_id = "${yandex_vpc_network.ya-network.id}"
+  network_id = "${nebius_vpc_network.ya-network.id}"
   v4_cidr_blocks = ["192.168.1.16/28"]
 }
 
-resource "yandex_vpc_subnet" "ya-sqlserver-rc1c" {
+resource "nebius_vpc_subnet" "ya-sqlserver-rc1c" {
   name       = "ya-sqlserver-rc1c"
   zone       = var.zone-c
-  network_id = "${yandex_vpc_network.ya-network.id}"
+  network_id = "${nebius_vpc_network.ya-network.id}"
   v4_cidr_blocks = ["192.168.1.32/28"]
 }
 
-resource "yandex_vpc_subnet" "ya-ilb-rc1a" {
+resource "nebius_vpc_subnet" "ya-ilb-rc1a" {
   name       = "ya-ilb-rc1a"
   zone       = var.zone-a
-  network_id = "${yandex_vpc_network.ya-network.id}"
+  network_id = "${nebius_vpc_network.ya-network.id}"
   v4_cidr_blocks = ["192.168.1.48/28"]
 }
 
-resource "yandex_vpc_subnet" "ya-ad-rc1a" {
+resource "nebius_vpc_subnet" "ya-ad-rc1a" {
   name       = "ya-ad-rc1a"
   zone       = var.zone-a
-  network_id = "${yandex_vpc_network.ya-network.id}"
+  network_id = "${nebius_vpc_network.ya-network.id}"
   v4_cidr_blocks = ["10.0.0.0/28"]
 }
 
 /*creating Network Load Balancer Target Group*/
-resource "yandex_lb_target_group" "ya-tg" {
+resource "nebius_lb_target_group" "ya-tg" {
   name      = "ya-tg"
   region_id = var.region_id
 
   target {
-    subnet_id = "${yandex_vpc_subnet.ya-sqlserver-rc1a.id}"
+    subnet_id = "${nebius_vpc_subnet.ya-sqlserver-rc1a.id}"
     address   = var.vm-ya-mssql1-internal-ipv4
   }
 
   target {
-    subnet_id = "${yandex_vpc_subnet.ya-sqlserver-rc1b.id}"
+    subnet_id = "${nebius_vpc_subnet.ya-sqlserver-rc1b.id}"
     address   = var.vm-ya-mssql2-internal-ipv4
   }
 
   target {
-    subnet_id = "${yandex_vpc_subnet.ya-sqlserver-rc1c.id}"
+    subnet_id = "${nebius_vpc_subnet.ya-sqlserver-rc1c.id}"
     address   = var.vm-ya-mssql3-internal-ipv4
   }
 }
 
 /*creating Network Load Balancer*/
-resource "yandex_lb_network_load_balancer" "ya-loadbalancer" {
+resource "nebius_lb_network_load_balancer" "ya-loadbalancer" {
   name = "ya-loadbalancer"
   type = "internal"
   listener {
@@ -146,12 +145,12 @@ resource "yandex_lb_network_load_balancer" "ya-loadbalancer" {
     protocol = "tcp"
     internal_address_spec {
       ip_version = "ipv4"
-      subnet_id = yandex_vpc_subnet.ya-ilb-rc1a.id
+      subnet_id = nebius_vpc_subnet.ya-ilb-rc1a.id
     }
   }
 
   attached_target_group {
-    target_group_id = "${yandex_lb_target_group.ya-tg.id}"
+    target_group_id = "${nebius_lb_target_group.ya-tg.id}"
 
     healthcheck {
       name = "listener"
@@ -164,7 +163,7 @@ resource "yandex_lb_network_load_balancer" "ya-loadbalancer" {
 
 
 /*creating VM ya-jump1 - Bastion Host witn NAT*/
- resource "yandex_compute_instance" "ya-jump1" {
+ resource "nebius_compute_instance" "ya-jump1" {
   name        = "ya-jump1"
   hostname    = "ya-jump1"
   platform_id = var.platform_id
@@ -184,7 +183,7 @@ resource "yandex_lb_network_load_balancer" "ya-loadbalancer" {
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.ya-ad-rc1a.id
+    subnet_id = nebius_vpc_subnet.ya-ad-rc1a.id
     nat       = true
   }
 
@@ -194,7 +193,7 @@ resource "yandex_lb_network_load_balancer" "ya-loadbalancer" {
 }
 
 /*creating VM ya-ad - Active Directory Server*/
- resource "yandex_compute_instance" "ya-ad" {
+ resource "nebius_compute_instance" "ya-ad" {
   name        = "ya-ad"
   hostname    = "ya-ad"
   platform_id = var.platform_id
@@ -214,7 +213,7 @@ resource "yandex_lb_network_load_balancer" "ya-loadbalancer" {
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.ya-ad-rc1a.id
+    subnet_id = nebius_vpc_subnet.ya-ad-rc1a.id
     ip_address = var.vm-ya-ad-internal-ipv4
     nat       = false
   }
@@ -225,14 +224,14 @@ resource "yandex_lb_network_load_balancer" "ya-loadbalancer" {
 }
 
 /*creating VM ya-mssql1 - MSSQL Server 2022 Cluster Noder*/
-resource "yandex_compute_disk" "ya-mssql1-db-disk" {
+resource "nebius_compute_disk" "ya-mssql1-db-disk" {
   name       = "ya-mssql1-db-disk"
   type       = "network-ssd"
   zone       = var.zone-a
   size       = 200
 }
 
- resource "yandex_compute_instance" "ya-mssql1" {
+ resource "nebius_compute_instance" "ya-mssql1" {
   name        = "ya-mssql1"
   hostname    = "ya-mssql1"
   platform_id = var.platform_id
@@ -252,13 +251,13 @@ resource "yandex_compute_disk" "ya-mssql1-db-disk" {
   }
 
   secondary_disk {
-    disk_id = yandex_compute_disk.ya-mssql1-db-disk.id
+    disk_id = nebius_compute_disk.ya-mssql1-db-disk.id
     mode = "READ_WRITE"
     auto_delete = true
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.ya-sqlserver-rc1a.id
+    subnet_id = nebius_vpc_subnet.ya-sqlserver-rc1a.id
     nat       = true
     ip_address = var.vm-ya-mssql1-internal-ipv4
   }
@@ -269,14 +268,14 @@ resource "yandex_compute_disk" "ya-mssql1-db-disk" {
 }
 
 /*creating VM ya-mssql2 - MSSQL Server 2022 Cluster Noder*/
-resource "yandex_compute_disk" "ya-mssql2-db-disk" {
+resource "nebius_compute_disk" "ya-mssql2-db-disk" {
   name       = "ya-mssql2-db-disk"
   type       = "network-ssd"
   zone       = var.zone-b
   size       = 200
 }
 
- resource "yandex_compute_instance" "ya-mssql2" {
+ resource "nebius_compute_instance" "ya-mssql2" {
   name        = "ya-mssql2"
   hostname    = "ya-mssql2"
   platform_id = var.platform_id
@@ -296,13 +295,13 @@ resource "yandex_compute_disk" "ya-mssql2-db-disk" {
   }
 
   secondary_disk {
-    disk_id = yandex_compute_disk.ya-mssql2-db-disk.id
+    disk_id = nebius_compute_disk.ya-mssql2-db-disk.id
     mode = "READ_WRITE"
     auto_delete = true
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.ya-sqlserver-rc1b.id
+    subnet_id = nebius_vpc_subnet.ya-sqlserver-rc1b.id
     nat       = true
     ip_address = var. vm-ya-mssql2-internal-ipv4
   }
@@ -313,14 +312,14 @@ resource "yandex_compute_disk" "ya-mssql2-db-disk" {
 }
 
 /*creating VM ya-mssql3 - MSSQL Server 2022 Cluster Noder*/
-resource "yandex_compute_disk" "ya-mssql3-db-disk" {
+resource "nebius_compute_disk" "ya-mssql3-db-disk" {
   name       = "ya-mssql3-db-disk"
   type       = "network-ssd"
   zone       = var.zone-c
   size       = 200
 }
 
- resource "yandex_compute_instance" "ya-mssql3" {
+ resource "nebius_compute_instance" "ya-mssql3" {
   name        = "ya-mssql3"
   hostname    = "ya-mssql3"
   platform_id = var.platform_id
@@ -340,13 +339,13 @@ resource "yandex_compute_disk" "ya-mssql3-db-disk" {
   }
 
   secondary_disk {
-    disk_id = yandex_compute_disk.ya-mssql3-db-disk.id
+    disk_id = nebius_compute_disk.ya-mssql3-db-disk.id
     mode = "READ_WRITE"
     auto_delete = true
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.ya-sqlserver-rc1c.id
+    subnet_id = nebius_vpc_subnet.ya-sqlserver-rc1c.id
     nat       = true
     ip_address = var. vm-ya-mssql3-internal-ipv4
   }
